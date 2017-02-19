cb9b33429c659cb9405f982bd39dda83b42a382a
Shameek Poddar <shameek.poddar@gmail.com>
2017-02-19 16:04:24 -0500

    Added function to make change

--- HelloChange.groovy
+++ HelloChange.groovy
@@ -1,39 +1,86 @@
 
 class Register {
 
-	Map<String, Integer> cash = [
-		twenties: 0,
-		tens: 0,
-		fives: 0,
-		twos: 0,
-		ones: 0,
+	//Map of bill value -> number of bills currently in register
+	def cash = [
+		(20): 0,
+		(10): 0,
+		(5): 0,
+		(2): 0,
+		(1): 0,
 	]
 	
-	def put(Map<String, Integer> cashMap){
+	//Given a map of bill values -> number of bills, adds them to what's currently in the register
+	def put(def cashMap){
 		cashMap.each { k, v -> 
 			cash[k] += cashMap[k]
 		}
 	}
 	
-	def take(Map<String, Integer> cashMap){
+	//Given a map of bill values -> number of bills, subtracts them from what's currently in the register
+	def take(def cashMap){
 		cashMap.each { k, v -> 
 			cash[k] -= cashMap[k]
 		}
 	}
 	
+	//Gets the total value given a map of bill values and number of bills
+	//Uses separate method to calculate total because we also calculate total when getting change
 	Integer getTotal(){
-		(cash.ones * 1) + (cash.twos * 2) + (cash.fives * 5) + (cash.tens * 10) + (cash.twenties * 20)
+		calcTotal(cash)
 	}
 	
 	String toString(){
 		"\$${getTotal()} ${cash.values().join(' ')}"
 	}
-
+	
+	def calcTotal(def cashMap){
+		//Returns the sum of each key * each value by recursively calling the closure against the map
+		//Advantage of this approach is that you can remove or add keys from the map without breaking anything
+		cashMap.inject(0){ total, denomination, bills -> total += denomination * bills } 
+	}
+	
+	//Details for change method
+	//This is based on the naive human way of making change - see if you have enough $5s, then enough $2s, then enough $1s
+	//Returns map of bills -> number of bills, and returns an empty map if unsuccessful
+	//Return value of this method can be passed to take
+	def getChange(def amount){
+		def change = [:]
+		def amountToGet = amount //A copy of amount that we can mutate when we pass through the loop
+		def denominations = cash.keySet().findAll { it <= amount }.sort() //Sort denominations in descending order so you can get the max by popping items off the list
+		
+		//Bail early if amount is more than what's currently in the register
+		if(amount > getTotal()){
+			println "Sorry, I don't have change for \$$amount"
+			return [:]
+		}
+		
+		while(!denominations.empty){ //Check every kind of bill that could make change
+			def totalChange = calcTotal(change)
+			if(totalChange == amount){
+				return change
+			}
+		
+			def bill = denominations.pop() //Get largest unexamined bill 
+			if((bill * cash[bill]) >= amountToGet){ //If we have more than enough change, take as much as we need
+				change.put(bill, Math.floor(amountToGet/bill) as Integer)
+			} else { //Otherwise, take every bill in the register and move on to the next smaller bill
+				change.put(bill, cash[bill]) 
+				amountToGet -= (bill * cash[bill])
+			}
+		}
+		
+		if(calcTotal(change) == amount){ //If what's in the change map equals the original amount, you succeeded!
+			return change
+		} else {
+			println "Sorry, I don't have change for \$$amount"
+			return [:]
+		}
+	}
 }
 
 def main(){
 	def register = new Register()
-	//register.put(ones: 2, twos: 1, fives: 1, tens: 1, twenties: 1)
 
 	def input = ""
 	while((input = System.console().readLine("> ")) != "quit"){ //While user hasn't quit
@@ -45,17 +92,26 @@ def main(){
 			case "put":
 			case "take":
 				if(args.size() < register.cash.size()){
-					println "You haven't specified the right amount of arguments"
+					println "You need to fill every slot in the register"
 				} else {
 					args = args.collect { it as Integer } //Cast all arguments to integers
 					register."$command"(
-						twenties: args[0],
-						tens: args[1],
-						fives: args[2],
-						twos: args[3],
-						ones: args[4],
+						20: args[0],
+						10: args[1],
+						5: args[2],
+						2: args[3],
+						1: args[4],
 					)
 				}
+				println register
+				break
+			case "change":
+				if(args.size() > 1){
+					println "You can only get change for one thing at a time"
+				}
+				def amount = args[0] as Integer
+				def change = register.getChange(amount)
+				register.take(change)
 				println register
 				break
 		}
